c1f1fe0da0fd334607a6bea4938ada45526e1cdc
Simas <simas@simas-dei.com>
2021-04-24 16:05:09 +0300

    initial setup 1/?

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var color = {
+    red: 10,
+    blue: 10,
+    green: 10,
+};
+console.log('🚀 ~ file: index.ts ~ line 8 ~ color', color);
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 10,
+  green: 10,
+};
+console.log('🚀 ~ file: index.ts ~ line 8 ~ color', color);
